e4ac936dc96fa4481d9e442ff6512ffda6562843
richard-shepherd <richard.s.shepherd@gmail.com>
2024-01-23 20:25:15 +0000

    alt - another log tailer

--- LogFileGenerator/Program.cs
+++ LogFileGenerator/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace LogFileGenerator
 {
@@ -10,11 +12,12 @@ namespace LogFileGenerator
     {
         // Log generation paramaters...
         static string PATH = "../../../LogFiles/LogFile1.log";
-        static int NUM_LINES = 100000;
-        static double MAX_SECONDS_BETWEEN_LINES = 1.0;
+        static int NUM_LINES = 25000000;
+        static double MAX_SECONDS_BETWEEN_LINES = 0.01;
         static DateTime START_TIMESTAMP = new DateTime(2024, 1, 10);
         static double ERROR_PROBABILITY = 0.01;
         static double WARN_PROBABILITY = 0.03;
+        static double CAPITAL_NOUN_PROBABILITY = 0.2;
 
         /// <summary>
         /// Main.
@@ -23,18 +26,41 @@ namespace LogFileGenerator
         {
             // We make sure the folder exists...
             Directory.CreateDirectory(Path.GetDirectoryName(PATH));
+            File.Create(PATH).Close();
 
             // We write log lines to the file with the params specified above...
             var timestamp = START_TIMESTAMP;
-            using (var filestream = File.OpenWrite(PATH))
-            using (var streamWriter = new StreamWriter(filestream))
+            using (var streamWriter = new StreamWriter(PATH))
             {
                 for (var i = 0; i < NUM_LINES; i++)
                 {
-                    streamWriter.WriteLine($"{timestamp:yyyy-MM-dd:HH:mm:ss.fff}: {logLevel()}: Line {i}");
+                    streamWriter.WriteLine(createLogLine(timestamp));
                     timestamp += TimeSpan.FromSeconds(m_rnd.NextDouble() * MAX_SECONDS_BETWEEN_LINES);
+                    if(i%1000 == 0)
+                    {
+                        Console.Write(".");
+                    }
                 }
             }
+        }
+
+        /// <summary>
+        /// Creates a log line with random information for the timestamp provided.
+        /// </summary>
+        static string createLogLine(DateTime timestamp)
+        {
+            var line = new StringBuilder();
+
+            // Timestamp...
+            line.Append($"{timestamp:yyyy-MM-dd HH:mm:ss.fff} ");
+
+            // Log level...
+            line.Append($"{logLevel()}: ");
+
+            // Text...
+            line.Append($"{phrase(true)} by {phrase(false)} from {shortPhrase()} with {shortPhrase()}");
+
+            return line.ToString();
         }
 
         /// <summary>
@@ -54,7 +80,95 @@ namespace LogFileGenerator
             return "INFO";
         }
 
+        /// <summary>
+        /// Returns a phrase verb-adjective-noun.
+        /// </summary>
+        static string phrase(bool initialCapital)
+        {
+            return $"{verb(initialCapital)} {adjective()} {noun()}";
+        }
+
+        /// <summary>
+        /// Returns a phrase adjective-noun.
+        /// </summary>
+        static string shortPhrase()
+        {
+            return $"{adjective()} {noun()}";
+        }
+
+        /// <summary>
+        /// Returns a random verb.
+        /// </summary>
+        static string verb(bool initialCapital)
+        {
+            var verb = m_verbs[m_rnd.Next(m_verbs.Count)];
+            if(initialCapital)
+            {
+                verb = verb[0].ToString().ToUpper() + verb.Substring(1);
+            }
+            return verb;
+        }
+
+        /// <summary>
+        /// Returns a random noun.
+        /// </summary>
+        static string noun()
+        {
+            var noun = m_nouns[m_rnd.Next(m_nouns.Count)];
+            if(m_rnd.NextDouble() < CAPITAL_NOUN_PROBABILITY)
+            {
+                noun = noun.ToUpper();
+            }
+            return noun;
+        }
+
+        /// <summary>
+        /// Returns a random adjective.
+        /// </summary>
+        static string adjective()
+        {
+            return m_adjectives[m_rnd.Next(m_adjectives.Count)];
+        }
+
         // Used for random time intervals bewteen lines, random log levels etc...
         static Random m_rnd = new Random();
+
+        // Lists of strings which can be combined to make log lines...
+        static List<string> m_verbs = new List<string>
+        {
+            "processing",
+            "retrieving",
+            "calculating",
+            "cross-referencing",
+            "inverting",
+            "storing"
+        };
+        static List<string> m_nouns = new List<string>
+        {
+            "data",
+            "payments",
+            "packets",
+            "pages",
+            "information",
+            "penguins",
+            "database"
+        };
+        static List<string> m_adjectives = new List<string>
+        {
+            "customer",
+            "rapid",
+            "inventory",
+            "up-to-date",
+            "latest",
+            "historic",
+            "failed",
+            "queried",
+            "discarded",
+            "obsolete",
+            "deprecated",
+            "meta"
+        };
+
+
     }
 }
